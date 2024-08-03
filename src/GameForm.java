140e4d0b3967ac9a282137b4e2e113384cba4af4
JesseBarnett24 <messijessi58@gmail.com>
2024-08-04 09:23:39 +1000

    0909

--- src/GameForm.java
+++ src/GameForm.java
@@ -1,3 +1,23 @@
-public class GameForm {
 
+import javax.swing.*;
+
+public class GameForm extends JPanel{
+    private JPanel MyGameForm;
+    private JPanel GameArea;
+    public JPanel CardPanel;
+
+    public GameForm() {
+        setContentPane(MyGameForm);
+        setContentPane(GameArea);
+        setTitle("Game Form");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new GameForm();
+
+    }
 }
