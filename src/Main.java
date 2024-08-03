a68855ff3e962377c9bc6bec4c326511ee7d1f4d
Basil Aziz <sbasilaziz2000@gmail.com>
2024-08-03 12:20:43 +1000

    Back to 1090

--- src/Main.java
+++ src/Main.java
@@ -37,7 +37,7 @@ public class Main {
 
         frame.getContentPane().add(panel);
 
-        frame.setSize(1920, 1080);
+        frame.setSize(1920, 1090);
         frame.setVisible(true);
     }
 }
