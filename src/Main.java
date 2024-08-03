e3398646948c3d06463f6603ac5d8f4cce812285
JesseBarnett24 <messijessi58@gmail.com>
2024-08-03 12:26:25 +1000

    1060

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
         frame.getContentPane().add(panel);
 
-        frame.setSize(1920, 1060);
+        frame.setSize(1920, 1000);
         frame.setVisible(true);
     }
 }
