824d6e6e243fd7a60be5e9543315bcb473cf04cb
Basil Aziz <sbasilaziz2000@gmail.com>
2024-08-03 13:22:06 +1000

    Main menu

---
+++ src/Main_menu.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+
+public class Main_menu {
+    public JPanel main_menu;
+
+    // Constructor for the main menu class
+    public Main_menu(){
+        main_menu = new JPanel();
+
+        JButton Play = new JButton();
+        JButton Configuration = new JButton();
+        JButton Highscores  = new JButton();
+        JButton Exit =
+
+    }
+
+
+
+}
