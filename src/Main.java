200f5a0846b44a6247d56cd4da0f469843da47b9
Basil Aziz <sbasilaziz2000@gmail.com>
2024-08-03 12:36:31 +1000

    3333

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
         frame.getContentPane().add(panel);
 
-        frame.setSize(1920, 1111);
+        frame.setSize(1920, 3333);
         frame.setVisible(true);
     }
 }
