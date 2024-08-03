80c65a716c8ae1a13b9805bd49ce588a8e9f0590
JesseBarnett24 <messijessi58@gmail.com>
2024-08-03 14:20:28 +1000

    0909

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 
-class LineDrawing extends JComponent {
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawLine(100, 100, 100, 500);
-        g.drawLine(400, 100, 400, 500);
-        g.drawLine(100,500, 400,500);
-    }
-
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(500, 600); // Set the preferred size of LineDrawing
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("My first JFrame");
