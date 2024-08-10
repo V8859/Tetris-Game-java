49d46b5e8a4c269830fe803b9b8b90a7bf926d47
JesseBarnett24 <messijessi58@gmail.com>
2024-08-10 19:12:27 +1000

    0909

--- src/gamePanel.java
+++ src/gamePanel.java
@@ -20,16 +20,50 @@ public class gamePanel extends JPanel{
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Define the size of the grid cells
+        drawBoard(g);
+        drawScore(g);
+        // Example: Drawing a sample piece at a fixed position
+        int[][] currentPiece = {
+                {1, 1},
+                {1, 1}
+        };
+        int pieceX = 4;  // Position on the board (column)
+        int pieceY = 0;  // Position on the board (row)
+
+        drawPiece(g, currentPiece, pieceX, pieceY);
+    }
+
+    private void drawBoard(Graphics g) {
         int cellSize = 30;  // Each cell is 30x30 pixels
 
         // Draw the grid
         g.setColor(Color.GRAY);  // Set color for grid lines
-        for (int x = 0; x <= this.boardWidth; x++) {
-            g.drawLine(x * cellSize, 0, x * cellSize, this.boardHeight * cellSize); // Vertical lines
+        for (int x = 0; x <= boardWidth; x++) {
+            g.drawLine(x * cellSize, 0, x * cellSize, boardHeight * cellSize); // Vertical lines
         }
-        for (int y = 0; y <= this.boardHeight; y++) {
-            g.drawLine(0, y * cellSize, this.boardWidth * cellSize, y * cellSize); // Horizontal lines
+        for (int y = 0; y <= boardHeight; y++) {
+            g.drawLine(0, y * cellSize, boardWidth * cellSize, y * cellSize); // Horizontal lines
+        }
+    }
+
+    private void drawScore(Graphics g) {
+        int score = 12345;  // Example score
+
+        g.setColor(Color.WHITE);  // Set color for the text
+        g.setFont(new Font("Arial", Font.BOLD, 20));  // Set font for the score
+        g.drawString("Score: " + score, 350, 30);  // Draw score at position (350, 30)
+    }
+
+    private void drawPiece(Graphics g, int[][] piece, int x, int y) {
+        g.setColor(Color.RED);  // Set the color for the piece
+        int cellSize = 30;  // Same size as defined earlier
+
+        for (int i = 0; i < piece.length; i++) {
+            for (int j = 0; j < piece[i].length; j++) {
+                if (piece[i][j] != 0) {
+                    g.fillRect((x + j) * cellSize, (y + i) * cellSize, cellSize, cellSize);
+                }
+            }
         }
     }
 }
