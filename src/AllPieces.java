ee89893b8279ce4a51cae903358108acecd839f1
Basil Aziz <sbasilaziz2000@gmail.com>
2024-08-11 16:20:41 +1000

    All the shapes are completed now I believe.

--- src/AllPieces.java
+++ src/AllPieces.java
@@ -2,7 +2,7 @@ public class AllPieces {
     // T piece
     public static final int[][][] T_SHAPE = {
             {{0, 1, 0},                // 0 degrees
-                    {1, 1, 1}},
+                {1, 1, 1}},
             {{1, 0},                   // 90 degrees
                     {1, 1},
                     {1, 0}},
@@ -12,5 +12,87 @@ public class AllPieces {
                     {1, 1},
                     {0, 1}}
     };
+
+    public static final int[][][] Z_SHAPE = {
+            // Can only be rotated two different ways, the other shape is the S_SHAPE
+
+            {{1,1,0},{0,1,1}}  // 0 degrees
+            ,{{1,0},{1,1}, {0,1}} // 90 degrees Alternate;
+    };
+    public static final int[][][] S_SHAPE = {
+            {{0,1,1},{1,1,0}} // 0 degrees
+            ,{{0,1},{1,1},{1,0}}
+    };
+
+    public static final int[][][] I_SHAPE = {
+            // Can only be rotated two different ways
+            {{1,1,1,1}}, // 0 degrees
+
+            {{1},
+             {1},
+             {1},
+             {1}} // 90 degrees
+
+            ,{{1,1,1,1}} // 180 degrees
+
+            ,{{1},
+              {1},
+              {1},
+              {1}}, //360 degrees
+    };
+    public static final int[][][] O_SHAPE = {
+            // No rotate possible for this shape
+            {{1,1},{1,1}},
+            {{1,1},{1,1}},
+            {{1,1},{1,1}},
+            {{1,1},{1,1}},
+    };
+
+    public static final int[][][] L_SHAPE = {
+            //// 0 degrees
+            {{1,0},
+             {1,0},
+             {1,1}},
+            //
+            // 90 degrees
+            {{0,0,1},
+             {1,1,1}},
+            //
+            //180 degrees
+            {{1,1},
+             {0,1},
+             {0,1}},
+            //
+            // 360 degrees
+            {{1,1,1},
+             {1,0,0}}
+            //
+    };
+    public static final int[][][] J_SHAPE = {
+            // 0 degrees
+            {{0,1},
+            {0,1},
+            {1,1}},
+            //
+
+            // 90 degrees
+            {{1,0,0},
+             {1,1,1}},
+            //
+
+            // 180 degrees
+            {{1,1},
+             {1,0},
+             {1,0}},
+            //
+
+            // 360 degrees
+            {{1,1,1},
+             {0,0,1}}
+            //
+
+
+    };
+
 
 }
